869c72edc61fd2fddb2638c8eef64a98eff65535
Ivo Katunaric <ikatun@gmail.com>
2019-08-12 14:44:26 +0200

    Move react-native to dev deps

--- build/index.js
+++ build/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
@@ -6,50 +6,32 @@ Object.defineProperty(exports, "__esModule", {
 exports.css = css;
 exports.styleSheet = styleSheet;
 
-var _lodash = require('lodash');
+var _lodash = require("lodash");
 
-var _reactNative = require('react-native');
+var _reactNative = require("react-native");
 
-var _cssToReactNative = require('css-to-react-native');
+var _cssToReactNative = _interopRequireDefault(require("css-to-react-native"));
 
-var _cssToReactNative2 = _interopRequireDefault(_cssToReactNative);
-
-var _cleanDeep = require('clean-deep');
-
-var _cleanDeep2 = _interopRequireDefault(_cleanDeep);
+var _cleanDeep = _interopRequireDefault(require("clean-deep"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var trim = function trim(x) {
-  return x.trim();
-};
-var merge = function merge(left, right) {
-  return (left === undefined ? '' : left) + (right === undefined ? '' : right);
-};
+const trim = x => x.trim();
 
-var transformationsToSkip = ['borderRadius'];
+const merge = (left, right) => (left === undefined ? '' : left) + (right === undefined ? '' : right);
+
+const transformationsToSkip = ['borderRadius'];
 
 function computeStyle(strings, args) {
-  var unzipped = (0, _lodash.unzipWith)([strings, args], merge).join('').split(';').map(trim).filter(_lodash.identity).map(function (line) {
-    return line.split(':').map(trim);
-  });
-
-  return (0, _cleanDeep2.default)((0, _cssToReactNative2.default)(unzipped, transformationsToSkip));
+  const unzipped = (0, _lodash.unzipWith)([strings, args], merge).join('').split(';').map(trim).filter(_lodash.identity).map(line => line.split(':').map(trim));
+  return (0, _cleanDeep.default)((0, _cssToReactNative.default)(unzipped, transformationsToSkip));
 }
 
-function css(strings) {
-  for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-    args[_key - 1] = arguments[_key];
-  }
-
+function css(strings, ...args) {
   return computeStyle(strings, args);
 }
 
-function styleSheet(strings) {
-  for (var _len2 = arguments.length, args = Array(_len2 > 1 ? _len2 - 1 : 0), _key2 = 1; _key2 < _len2; _key2++) {
-    args[_key2 - 1] = arguments[_key2];
-  }
-
+function styleSheet(strings, ...args) {
   return _reactNative.StyleSheet.create({
     style: computeStyle(strings, args)
   }).style;
