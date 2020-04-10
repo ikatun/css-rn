b1925552645df83e02cb9525e19c1e5e17595477
Ivo Katunaric <ikatun@gmail.com>
2020-04-10 17:11:03 +0200

    Rebuild

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,3 +1,5 @@
-import { StyleProp, ViewProps } from 'react-native';
-export declare function css(strings: any, ...args: any[]): StyleProp<ViewProps>;
-export declare function styleSheet(strings: any, ...args: any[]): StyleProp<ViewProps>;
+export declare function styleObject(strings: any, ...args: any[]): any;
+export declare function styleSheet(strings: any, ...args: any[]): any;
+export declare function useStyleObject(shouldMemoize: boolean): void;
+export declare function useStyleSheet(shouldMemoize: boolean): void;
+export declare function css(strings: any, ...args: any[]): any;
--- build/index.js
+++ build/index.js
@@ -7,6 +7,7 @@ const lodash_1 = require("lodash");
 const react_native_1 = require("react-native");
 const css_to_react_native_1 = __importDefault(require("css-to-react-native"));
 const clean_deep_1 = __importDefault(require("clean-deep"));
+const fast_memoize_1 = __importDefault(require("fast-memoize"));
 const trim = x => x.trim();
 const merge = (left, right) => (left === undefined ? '' : left) + (right === undefined ? '' : right);
 const transformationsToSkip = ['borderRadius'];
@@ -21,14 +22,38 @@ function computeStyle(strings, args) {
         .filter(([left, right]) => !isEmpty(left) && !isEmpty(right));
     return clean_deep_1.default(css_to_react_native_1.default(unzipped, transformationsToSkip));
 }
-function css(strings, ...args) {
+function styleObject(strings, ...args) {
     return computeStyle(strings, args);
 }
-exports.css = css;
+exports.styleObject = styleObject;
 function styleSheet(strings, ...args) {
     return react_native_1.StyleSheet.create({
         style: computeStyle(strings, args),
     }).style;
 }
 exports.styleSheet = styleSheet;
+let defaultStylingFunction = styleSheet;
+function useStyleObject(shouldMemoize) {
+    if (shouldMemoize) {
+        defaultStylingFunction = fast_memoize_1.default((...args) => styleObject(...args));
+    }
+    else {
+        defaultStylingFunction = styleObject;
+    }
+}
+exports.useStyleObject = useStyleObject;
+function useStyleSheet(shouldMemoize) {
+    if (shouldMemoize) {
+        defaultStylingFunction = fast_memoize_1.default((...args) => styleSheet(...args));
+    }
+    else {
+        defaultStylingFunction = styleSheet;
+    }
+}
+exports.useStyleSheet = useStyleSheet;
+useStyleSheet(true);
+function css(strings, ...args) {
+    return defaultStylingFunction(strings, args);
+}
+exports.css = css;
 //# sourceMappingURL=index.js.map
